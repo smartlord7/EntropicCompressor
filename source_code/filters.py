9caee3cf8152d30aa912077b15c5895aa1084eeb
Sancho <smartlord7@gmail.com>
2020-11-26 12:49:54 +0000

    Code: Fixed sub filter, added transpose functionality and test for pairs of symbols

--- source_code/filters.py
+++ source_code/filters.py
@@ -2,6 +2,7 @@ import numpy as np
 import lib.entropic_codification as ec
 import matplotlib.image as img
 import os
+import copy
 
 
 def to_numpy_int8(data):
@@ -12,20 +13,26 @@ def to_numpy_int8(data):
     return data
 
 
-def apply_subtraction_filter(data, subtract_previous=True):
-    data = to_numpy_int8(data)
+def apply_subtraction_filter(data, subtract_previous=True, transpose=False):
+    data_copy = to_numpy_int8(copy.deepcopy(data))
+    if transpose:
+        data_copy = np.transpose(data_copy)
+    data_copy = data_copy.flatten()
     if subtract_previous:
-        data_copy = np.delete(data.copy(), len(data) - 1)
-        data_copy = np.concatenate(([0], data_copy))
+        sub = np.delete(data_copy, len(data_copy) - 1)
+        sub = np.concatenate(([0], sub))
     else:
-        data_copy = np.delete(data, 0)
-        data_copy = np.concatenate((data_copy, [0]))
-    return data - data_copy
+        sub = np.delete(data, 0)
+        sub = np.concatenate((sub, [0]))
+    return data_copy - sub
 
 
-def decode_subtraction_filter(data, subtract_previous=True):
+def decode_subtraction_filter(data, subtract_previous=True, transpose=False):
     data = to_numpy_int8(data)
+    data = data.flatten()
     l = len(data)
+    if transpose:
+        data = np.transpose(data)
     if subtract_previous:
         for i in range(1, l):
             data[i] = data[i] + data[i - 1]
@@ -35,7 +42,7 @@ def decode_subtraction_filter(data, subtract_previous=True):
     return data
 
 
-FILES_DIR = 'C:\\Users\\Sancho\\PycharmProjects\\TI\TP2\\resources\\images\\uncompressed_images'
+FILES_DIR = '../resources/images/uncompressed_images/'
 
 
 def analyse_files(files_dir):
@@ -43,17 +50,18 @@ def analyse_files(files_dir):
         for file in files:
             if file.endswith('.bmp') or file.endswith('.jpg'):
                 image_data = img.imread(files_dir + '\\' + file)
-                image_data = image_data.flatten()
-                image_data_filtered = apply_subtraction_filter(image_data)
-                alphabet = ec.gen_alphabet(image_data)
-                len_data = len(image_data)
-                len_alphabet = len(alphabet)
-                histogram = ec.gen_histogram(image_data, len_alphabet)
-                histogram_filtered = ec.gen_histogram(image_data_filtered, len_alphabet)
-                print('Entropy (without subtraction filter): %.4f bits\n'
-                      'Entropy (with subtraction filter) : %.4f bits'
-                      % (ec.entropy(histogram, len_data),
-                         ec.entropy(histogram_filtered, len_data)))
+                image_data_filtered = apply_subtraction_filter(image_data, transpose=True)
+                len_data = image_data.shape[0] * image_data.shape[1]
+                histogram = np.array(np.unique(image_data, return_counts=True))[1]
+                histogram_filtered = np.array(np.unique(image_data_filtered, return_counts=True))[1]
+                histogram_grouped, num_groups = ec.gen_histogram_generic(image_data_filtered, 2)
+
+                print('%s:\nEntropy (without subtraction filter): %.4f bits\n'
+                      'Entropy (with subtraction filter) : %.4f bits\n'
+                      'Entropy (with subtraction filter and pairs of symbols) : %.4f bits\n'
+                      % (file, ec.entropy(histogram, len_data),
+                         ec.entropy(histogram_filtered, len_data),
+                         ec.entropy_generic(histogram_grouped, num_groups, 2)))
 
 
 def main():
