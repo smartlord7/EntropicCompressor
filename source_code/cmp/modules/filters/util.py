fd70faae187ac19f954b5df5abf029b7664abf43
Razor <tiagofilipe122@gmail.com>
2020-12-23 01:43:31 +0000

    Code Documentation corrections and Code Revision.

--- source_code/cmp/modules/filters/util.py
+++ source_code/cmp/modules/filters/util.py
@@ -4,8 +4,8 @@ import numpy as np
 def to_numpy_uint8(data):
     """
     Util function that converts an iterable to a numpy uint8 array.
-    :param data: the iterable to be converted
-    :return: the converted iterable
+    :param data: the iterable to be converted.
+    :return: the converted iterable.
     """
     if type(data) != np.ndarray:
         data = np.array(data).astype(np.uint8)
@@ -17,7 +17,7 @@ def show_progress(counter, length):
         Util function that shows in the console the current progress, in percentage, of
         the algorithm that is being applied on the data in question.
         :param counter: the current index in the data.
-        :param length: the data's length
+        :param length: the data's length.
         :return:
     """
     if not counter % 100000:
